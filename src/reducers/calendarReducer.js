d5c6b779387e6aea67bb07a25888c7c8ba251a4e
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-11-11 10:24:23 +0000

    add actions to reducer

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -3,7 +3,19 @@ const initState = {
 };
 
 const calendarReducer = (state = initState, action) => {
-    return state;
+    switch (action.type) {
+        case "loadMeetings":
+            return {
+                meetings: action.payload.meetings,
+            };
+        case "saveMeeting":
+            return {
+                ...state,
+                meetings: [...state.meetings, action.payload.newMeeting],
+            };
+        default:
+            return state;
+    }
 };
 
 export default calendarReducer;
