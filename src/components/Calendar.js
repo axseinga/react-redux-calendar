c908b9822be092ae4dd6001d274f4f83e91cf6f2
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-11-11 16:04:44 +0000

    delete comments and consolelogs

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,10 +2,8 @@ import React, { useEffect } from "react";
 
 import CalendarList from "./CalendarList";
 import CalendarForm from "./CalendarForm";
-/*import { connect } from "react-redux";*/
 import { useSelector, useDispatch } from "react-redux";
-/*import { loadMeetingsAction, saveMeetingAction } from "../actions/calendar";*/
-import { loadAPI, sendAPI } from "../providers/calendarProvider";
+import { loadAPI } from "../providers/calendarProvider";
 
 const Calendar = (props) => {
     const dispatch = useDispatch();
@@ -19,42 +17,10 @@ const Calendar = (props) => {
     return (
         <section>
             <CalendarList meetings={meetings} />
-            <CalendarForm /*saveMeeting={dispatch(sendAPI)}*/ />
+            <CalendarForm/>
         </section>
     );
 };
 
 export default Calendar;
 
-/*class Calendar extends React.Component {
-    meetingsAPI = new meetingsAPI(
-        this.props.loadMeetings,
-        this.props.saveMeeting
-    );
-
-    componentDidMount() {
-        this.meetingsAPI.load();
-    }
-
-    render() {
-        return (
-            <section>
-                <CalendarList meetings={this.props.meetings} />
-                <CalendarForm saveMeeting={this.meetingsAPI.send} />
-            </section>
-        );
-    }
-}
-
-const mapStateToProps = (state, props) => {
-    return {
-        meetings: state.meetings,
-    };
-};
-
-const mapActionToProps = {
-    loadMeetings: loadMeetingsAction,
-    saveMeeting: saveMeetingAction,
-};
-
-export default connect(mapStateToProps, mapActionToProps)(Calendar);*/
--- src/providers/calendarProvider.js
+++ src/providers/calendarProvider.js
@@ -13,7 +13,6 @@ export const loadAPI = () => {
                 throw new Error("Network error!");
             })
             .then((resp) => {
-                console.log(resp);
                 dispatch(loadMeetingsAction(resp));
             })
             .catch((err) => {
@@ -39,7 +38,6 @@ export const sendAPI = (meetingData) => {
                 throw new Error("Network error!");
             })
             .then((meetingData) => {
-                console.log(meetingData);
                 dispatch(saveMeetingAction(meetingData));
             })
             .catch((err) => {
@@ -47,55 +45,3 @@ export const sendAPI = (meetingData) => {
             });
     };
 };
-
-/*class meetingsAPI {
-    constructor(loadMeetings, saveMeeting) {
-        this.loadMeetings = loadMeetings;
-        this.saveMeeting = saveMeeting;
-    }
-
-    apiUrl = "http://localhost:3005/meetings";
-
-    load() {
-        fetch(this.apiUrl)
-            .then((resp) => {
-                if (resp.ok) {
-                    return resp.json();
-                }
-
-                throw new Error("Network error!");
-            })
-            .then((resp) => {
-                this.loadMeetings(resp);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }
-
-    send = (meetingData) => {
-        fetch(this.apiUrl, {
-            method: "POST",
-            body: JSON.stringify(meetingData),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((resp) => {
-                if (resp.ok) {
-                    return resp.json();
-                }
-
-                throw new Error("Network error!");
-            })
-            .then((meetingData) => {
-                console.log(meetingData);
-                this.saveMeeting(meetingData);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-}
-
-export default meetingsAPI;*/
